73fa601c264574e865eaf3f911f35ac79e2e0101
Ari205553 <Ari205553@student-schoolsofwestfield.org>
2024-12-18 17:07:30 +0000

    done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+function setUp(){
+    let buttons = document.getElementById("buttons");
+    let ok = document.createElement("button");
+    ok.innerHTML="ok";
+    ok.addEventListener("click" ,checkPassword)
+    buttons.appendChild(ok);
+    let cancel = document.createElement("button");
+    cancel.innerHTML = "cancel";
+    buttons.appendChild(cancel);
+}
+function checkPassword(){
+    let dialogs = document.getElementById("dialogs");
+    let userName = dialogs.querySelector(' :nth-child(2)').value;
+    let password = dialogs.querySelector(':nth-child(4)').value;
+}
