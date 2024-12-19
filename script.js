50e6e41f8ef6d46643d94fed6b3f1b053f6bef7b
Ari205553 <Ari205553@student-schoolsofwestfield.org>
2024-12-19 17:31:01 +0000

    done

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+CrUser= "Lawnmover Mann"
+CrPassword = "7.3942"
+
 function setUp(){
     let buttons = document.getElementById("buttons");
     let ok = document.createElement("button");
     ok.innerHTML="ok";
-    ok.addEventListener("click" ,checkPassword)
+    ok.addEventListener("click" ,checkPassword);
     buttons.appendChild(ok);
     let cancel = document.createElement("button");
     cancel.innerHTML = "cancel";
@@ -12,4 +15,17 @@ function checkPassword(){
     let dialogs = document.getElementById("dialogs");
     let userName = dialogs.querySelector(' :nth-child(2)').value;
     let password = dialogs.querySelector(':nth-child(4)').value;
+    let body = document.getElementByTagName("body")[0];
+    let box = document.getElementByClassName("box");
+    if (username == Cruser  &&  password == Crpassword) {
+      let box = document.getElementByClassName("box");
+      box[0].remove()
+        body.style.backgroundImage = "url ('http://i.imgur.com/IUVDNyp.jpg')"
+    else{
+    userName ="";
+    password = "";
+    body.style.backgroundColor = "red";
+
+    }
+}
 }
